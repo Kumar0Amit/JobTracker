d0c82ea067c7092e2c42bf6c168b8da0c30fb9b3
Kumar0Amit1 <amitmlkumar5689@gmail.com>
2025-09-04 22:33:17 +0530

    LOGO initals are fixed

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/images/Logo.png';
+import logo from '../assets/images/LOGO.png';
 
 function Logo({ width = 150 }) {
   return <img src={logo} alt="MyJobTracker" className="logo" style={{ width: `${width}px`, height: 'auto' }}/>;
